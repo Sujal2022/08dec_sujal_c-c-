bb3ec64634c254c1359239daf34376c7d092c04a
Sujal2022 <120082994+Sujal2022@users.noreply.github.com>
2022-12-14 15:11:10 +0530

    Add files via upload

---
+++ assigment/module 1/module 1.c
@@ -0,0 +1,9 @@
+//WAP INTRODUCTION
+#include<stdio.h>
+void main()
+{
+	printf("\n SUJAL KHUNT");
+	printf("\n 20 MARCH 2004");
+	printf("\n 18");
+	printf("\n SHREEJIKUNJ,CHANDRA PARK 1 \n STREET NO 2 BLOCK NO 22 \n150 FEET ROAD RAJKOT ");
+}
