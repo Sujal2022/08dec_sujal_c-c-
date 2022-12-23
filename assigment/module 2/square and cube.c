0698812b5685bab6f3cfade6c61a5232115db733
Sujal2022 <120082994+Sujal2022@users.noreply.github.com>
2022-12-23 10:59:33 +0530

    Add files via upload

---
+++ assigment/module 2/square and cube.c
@@ -0,0 +1,20 @@
+//WAP CUBE AND SQUARE
+#include<stdio.h>
+int main()
+{
+	int a,r,n;
+	printf("tne number is:");
+	scanf("%d",&r);
+	
+	a=r*r*r;
+	printf("\n--------cube-------");
+	printf("\ncube:%d",a);
+	
+	n=r*r;
+	printf("\n--------square--------");
+	printf("\nsquare:%d",n);
+	
+	return 0;
+}
+		
+	
