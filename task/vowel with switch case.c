287ac2cda0319f3b7c099c42189e1bd304b5e111
Sujal2022 <120082994+Sujal2022@users.noreply.github.com>
2023-01-16 10:53:24 +0530

    Add files via upload

---
+++ task/vowel with switch case.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	printf("enter a alphabet:");
+	scanf("%c",&c);
+	switch(c)
+	{
+		case 'a':
+			printf("vowels");
+			break;
+		case 'e':
+			printf("vowels");
+			break;
+		case 'i':
+			printf("vowels");
+			break;
+		case 'o':
+			printf("vowels");
+			break;
+		case 'u':
+			printf("vowels");
+			break;
+		case 'A':
+			printf("vowels");
+			break;
+		case 'E':
+			printf("vowels");
+			break;
+		case 'I':
+			printf("vowels");
+			break;
+		case 'O':
+			printf("vowels");
+			break;
+		case 'U':
+			printf("vowels");
+			break;
+		default:
+			printf("constant");									
+	}
+	 return 0;
+}
