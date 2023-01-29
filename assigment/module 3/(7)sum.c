ccb174e70bbe1f996d821c4694e235458175f4e1
Sujal2022 <120082994+Sujal2022@users.noreply.github.com>
2023-01-29 16:54:29 +0530

    Add files via upload

---
+++ assigment/module 3/(7)sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,t;
+	printf("enter a value:");
+	scanf("%d",&a);
+	printf("enter b value:");
+	scanf("%d",&b);
+	printf("enter c value:");
+	scanf("%d",&c);
+	printf("enter d value:");
+	scanf("%d",&d);
+	
+	t=a+b+c+d;
+	printf("summation:%d",t);
+}
