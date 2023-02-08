bac1d81c33d916a6a5ad1f6151335cb545310dec
Sujal2022 <120082994+Sujal2022@users.noreply.github.com>
2023-02-08 10:03:42 +0530

    Add files via upload

---
+++ assigment/module 4/read abd write.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fptr;
+    char str[20];
+    fptr = fopen("D:/module 4/read and write.txt", "r");
+    printf("%s",fgets(str,15,fptr));
+    fclose(fptr);
+    return 0;
+}
